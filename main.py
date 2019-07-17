dd540e178d1a17a1de6a81ed2fc9d121f4e8b460
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2019-07-17 11:43:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-import requests
-
-API_ROOT = 'https://www.alphavantage.co/'
-
-API_LOCATION = 'query?'
-
-function = '=TIME_SERIES_DAILY_ADJUSTED'
-
-symbol = '=input'
-
-output_size = '=compact'
-
-datatype = '=json'
-
-apikey = ''
-
-def main_script():
-
-if __name__ == '__main__':
-    while True:
-        main_script()
+API_KEY = 'demo'
+r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=' + API_KEY)
+if (r.status_code == 200):
+  print r.json()
