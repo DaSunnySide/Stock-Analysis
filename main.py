676e2f578f0e3c8f222a4836b4d2c25249c63d02
Adam Helton <helton93@protonmail.com>
2019-07-17 16:04:42 -0400

    new r request

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ def main_script():
     print(main_script)
 
 main_script()
+
+API_KEY = 'YOUR_API_KEY'
+r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=' + API_KEY)
+if (r.status_code == 200):
+    print(r)
