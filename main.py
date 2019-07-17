c9a83552d3887fb8fb558025233d91f11dcb6586
GeniusAlg0s <patrick.williams56@student.purdueglobal.edu>
2019-07-17 13:57:01 -0700

    print

--- main.py
+++ main.py
@@ -25,3 +25,12 @@ API_KEY = 'YOUR_API_KEY'
 r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=' + API_KEY)
 if (r.status_code == 200):
     print(r)
+
+    result = r.json()
+dataForAllDays = result['Time Series (Daily)']
+dataForSingleDate = dataForAllDays['2017-10-30']
+print dataForSingleDate['1. open']
+print dataForSingleDate['2. high']
+print dataForSingleDate['3. low']
+print dataForSingleDate['4. close']
+print dataForSingleDate['5. volume']
