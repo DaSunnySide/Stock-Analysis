53a6f6cf17853ad75da4b0c3d6144aeb1248340b
Adam Helton <helton93@protonmail.com>
2019-07-17 14:58:33 -0400

    Fixed formatting

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-API_KEY = 'demo'
-r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=' + API_KEY)
-if (r.status_code == 200):
-  print r.json()
-
-
 
 import requests
 
@@ -26,31 +19,10 @@ def main_script():
     print(main_script)
 
 main_script()
-||||||| merged common ancestors
-import requests
 
-API_ROOT = 'https://www.alphavantage.co/'
 
-API_LOCATION = 'query?'
-
-function = '=TIME_SERIES_DAILY_ADJUSTED'
-
-symbol = '=input'
-
-output_size = '=compact'
-
-datatype = '=json'
-
-apikey = ''
-
-def main_script():
-
-if __name__ == '__main__':
-    while True:
-        main_script()
-=======
 API_KEY = 'demo'
 r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=' + API_KEY)
 if (r.status_code == 200):
   print r.json()
->>>>>>> 96a1cacfa79ee2be8c42f08d86d4cbef77ce8a6f
+
