75a960b8e031fc4a75daffb2c4edb4896caf94e7
Joey Schmidt <49414795+DaSunnySide@users.noreply.github.com>
2019-07-21 10:38:04 -0700

    Add files via upload
    Test file with part of the gui

--- test.py
+++ test.py
@@ -1,5 +1,33 @@
-#Use this for something your not sure about/just want to see what it does. That way we can keep the code clean 
-from main import *
+import requests
+import json
+from tkinter import *
 
-print(symbol)
+API_KEY = "VW506K51LFXGUT1C"
+r = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=" + API_KEY)
+res = r.json()
+formatted = json.dumps(res, indent=4, sort_keys=True)
+
+root = Tk()
+title = Label(root, text="Mugger-Chilly", bg="gray", fg="white")
+title.grid(row=0)
+
+symbol = Entry(root)
+symbol.grid(row=0, column=1)
+
+info = json.load(formatted)
+textBox = Text(root, height=50, width=30)
+textBox.grid(row=1, column=1)
+textBox.insert(side=END, )
+
+
+def onClick(submitButton):
+    pass
+
+
+submitButton = Button(root, text="Submit", command=onClick)
+submitButton.grid(row=1, column=1)
+
+root.mainloop()
+
+
 
