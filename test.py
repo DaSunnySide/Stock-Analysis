3c6e75080aae81e707267c2d0780ba5f02a327a9
Adam Helton <helton93@protonmail.com>
2019-07-24 15:47:22 -0400

    gui corrections

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ from tkinter import *
 
 
 root = Tk()
-title = Label(root, text="Mugger-Chilly", bg="gray", fg="white")
+title = Label(root, text="Stock Symbol", bg="gray", fg="white")
 title.grid(row=0)
-textBox = Text(root, height=50, width=30)
+textBox = Text(root, height=2, width=30)
 textBox.grid(row=1, column=1)
 buttonCommit = Button(root, height=1, width=10, text="Commit",
                      command=lambda: retrieve_input())
 
 buttonCommit.grid(row=1, column=1)
 symbol = buttonCommit
-symbol.grid(row=0, column=1)
+symbol.grid(row=2, column=2)
 funct = input("Enter the function (TIME_SERIES_DAILY, TIME_SERIES_INTRADAY, \nTIME_SERIES_DAILY_ADJUSTED, TIME_SERIES_WEEKLY, TIME_SERIES_WEEKLY_ADJUSTED, \nTIME_SERIES_MONTHLY, TIME_SERIES_MONTHLY_ADJUSTED)")
 API_KEY = "VW506K51LFXGUT1C"
 API_ROOT = 'https://www.alphavantage.co/'
