9760b80ca3f2fe185930713b2c7466d899e5c1d4
Joey Schmidt <49414795+DaSunnySide@users.noreply.github.com>
2019-08-05 16:32:08 -0700

    Add files via upload
    Implements a simple dashboard

--- jsonpractice.py
+++ jsonpractice.py
@@ -1,5 +1,6 @@
 import plotly.graph_objs as go
 import pandas as pd
+import plotly.graph_objs as go
 import plotly.offline as pyo
 from pandas.io.json import json_normalize
 import plotly.tools as tools
@@ -7,17 +8,40 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 
+#Initialize App
+app = dash.Dash()
+
+#Get user input for stock choice
 symbol = input('Stock Symbol: \n')
 datatype = 'datatype=csv'
 
+
+#Define Api call
 API_KEY = 'VW506K51LFXGUT1C'
 API_QUERY = 'https://www.alphavantage.co/query?function=' + 'TIME_SERIES_DAILY_ADJUSTED' + '&symbol=' + symbol + '&' + 'apikey=' + API_KEY+'&' +datatype
 
+#Print API Call Url for debugging
 print(API_QUERY)
 
+#Define Dataframe
 df = pd.read_csv(API_QUERY)
-print(df)
-trace = go.Scatter(x=df['timestamp'],y=df['close'],mode='lines', name='stocks')
-layout=go.Layout(title="Stock prices over time")
-fig = go.Figure(data=trace, layout=layout)
-pyo.plot(fig)
+
+app.layout = html.Div(([
+    dcc.Graph(id='linegraph',
+              figure={
+                  'data': [
+                      go.Scatter(x=df['timestamp'],y=df['close'],
+                                 mode='lines', marker= {
+                              'size' : 12
+                          })],
+                   'layout' : go.Layout(title='Stock prices over time')})
+])
+)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server()
